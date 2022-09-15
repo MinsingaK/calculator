0f397658467550dab8edfc0ce755a9c5eb331020
MinsingaK <minsingapepito@gmail.com>
2022-09-16 01:23:46 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="shortcut icon" href="images/alien-153542__340.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <title>My calculator</title>
+</head>
+<body>
+    <section class="content_box">
+        <h2>Ma calculatrice</h2>
+        <div class="affichage">
+            
+        </div>
+        <div class="content">
+            <div class="content_header">
+                <button class="number">C</button>
+                <button class="number">/</button>
+                <button class="number">x</button>
+                <button class="number">dlt</button>
+            </div>
+            <div class="card">
+                <div class="content_number">
+                    <button class="number">1</button>
+                    <button class="number">2</button>
+                    <button class="number">3</button>
+                    <button class="number">4</button>
+                    <button class="number">5</button>
+                    <button class="number">6</button>
+                    <button class="number">7</button>
+                    <button class="number">8</button>
+                    <button class="number">9</button>
+                    <button class="number">0</button>
+                    <button class="number">,</button>             
+                </div>
+                <div class="content_aside">
+                    <button class="number" id="moins">-</button>
+                    <button class="number" id="plus">+</button>
+                    <button class="number" id="egal">=</button>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
